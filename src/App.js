552bb38de550dfd96abd2558b09988890e030b70
mi929lee <mi929lee@gmail.com>
2021-08-08 02:22:12 -0700

    added name, abilities, weight, height in search results

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ const App = () => {
   const [temp, setTemp] = useState();
   const [img, setImg] = useState();
 
+  const [name, setName] = useState();
+
+  const [firstAbility, setFirstAbility] = useState();
+  const [secondAbility, setSecondAbility] = useState();
+
   const [hp, setHp] = useState();
   const [attack, setAttack] = useState();
   const [defense, setDefense] = useState();
   const [specialAttack, setSpecialAttack] = useState();
   const [specialDefense, setSpecialDefense] = useState();
   const [speed, setSpeed] = useState();
+
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
 
   useEffect(() => {
     (async () => {
@@ -42,13 +50,25 @@ const App = () => {
     console.log(json);
     setImg(json.sprites.front_shiny);
 
-    console.log(json.stats[0].base_stat);
+    setName(json.species.name);
+
     setHp(json.stats[0].base_stat);
     setAttack(json.stats[1].base_stat);
     setDefense(json.stats[2].base_stat);
     setSpecialAttack(json.stats[3].base_stat);
     setSpecialDefense(json.stats[4].base_stat);
     setSpeed(json.stats[5].base_stat);
+
+    setFirstAbility(json.abilities[0].ability.name);
+    console.log(json.abilities.length);
+    if (json.abilities.length != 2) {
+      setSecondAbility("n/a");
+    } else {
+      setSecondAbility(json.abilities[1].ability.name);
+    }
+
+    setWeight(json.weight);
+    setHeight(json.height);
   };
 
   return (
@@ -57,7 +77,7 @@ const App = () => {
       <p>
         <strong>search for a pokémon</strong> by entering one of the following:
         <ul>
-          <li>index number</li>
+          <li>index number (1-898)</li>
           <li>name</li>
         </ul>
         you will be able to view an <strong>image</strong> of the pokémon as
@@ -65,23 +85,52 @@ const App = () => {
       </p>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input type="text" required />
         <button type="submit">Search</button>
       </form>
 
       <h3>search result: {field}</h3>
-      
-      {img && <img src={img} />}
-      <p><strong>stats</strong></p>
-      <ul>
-        <li>hp: {hp}</li>
-        <li>attack: {attack}</li>
-        <li>defense: {defense}</li>
-        <li>special attack: {specialAttack}</li>
-        <li>special defense: {specialDefense}</li>
-        <li>speed: {speed}</li>
-      </ul>
-      
+
+      {img && (
+        <div className="description">
+          <img src={img} />
+          <p>
+            <strong>name</strong>: {name}
+            <br />
+            <br />
+            <strong>abilities</strong>
+            <ol>
+              <li>{firstAbility}</li>
+              <li>{secondAbility}</li>
+            </ol>
+            <strong>stats</strong>
+            <ul>
+              <li>
+                <u>hp</u>: {hp}
+              </li>
+              <li>
+                <u>attack</u>: {attack}
+              </li>
+              <li>
+                <u>defense</u>: {defense}
+              </li>
+              <li>
+                <u>special attack</u>: {specialAttack}
+              </li>
+              <li>
+                <u>special defense</u>: {specialDefense}
+              </li>
+              <li>
+                <u>speed</u>: {speed}
+              </li>
+            </ul>
+            <strong>weight</strong>: {weight}
+            <br />
+            <strong>height</strong>: {height}
+          </p>
+        </div>
+      )}
+
       <hr />
       <h3>example pokémon images</h3>
       {temp ? (
