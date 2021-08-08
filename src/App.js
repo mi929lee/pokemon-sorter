f42cb311018f20b3d700ed10c9a96fa5868bf26b
mi929lee <mi929lee@gmail.com>
2021-08-08 00:56:01 -0700

    added feature to search pokemon by index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [field, setField] = useState();
+  const [temp, setTemp] = useState();
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+      );
+      const json = await res.json();
+      const urls = await json.results.map((item) => item.url);
+      // console.log(urls);
+      const images = await Promise.all(
+        urls.map(async (url) => {
+          const res = await fetch(url);
+          const json = await res.json();
+          return json.sprites.front_shiny;
+        })
+      );
+      // console.log(images);
+      setTemp(images);
+    })();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setField(e.target[0].value);
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${e.target[0].value}`
+    );
+    const json = await res.json();
+    // console.log(json);
+    setImg(json.sprites.front_shiny);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>type in a <strong>pokemon index number</strong> to view the pokemon</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <button type="submit">Search</button>
+      </form>
+      <p>number: {field}</p>
+      {img && <img src={img} />}
+      <hr />
+      {temp ? (
+        temp.map((item, idx) => <img key={idx} src={item} />)
+      ) : (
+        <p>Not loaded yet</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
